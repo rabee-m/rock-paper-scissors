e5d39dbb8755ec241664cc0dc52d700e5edad4d2
Rabee <rabee@Talals-MacBook-Air.local>
2022-06-15 12:39:37 -0400

    add functionality  to play the game using console and browser prompts

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
 function computerPlay() {
     let choices = ['rock', 'paper', 'scissors'];
     compChoice = choices[Math.floor(Math.random() * choices.length)];
@@ -21,6 +25,44 @@ function getRoundResult(playerSelection, computerSelction) {
     }
 }
 
-function playRound(playerSelection, computerSelction) {
-    let roundResult = getRoundResult(playerSelection, computerSelction)
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase(); //make playerSelection case insensitive
+    let roundResult = getRoundResult(playerSelection, computerSelection);
+
+    //For formatting purposes, capitalize first letter of strings
+    formattedRoundResult = capitalizeFirstLetter(roundResult);
+    formattedPlayerSelection = capitalizeFirstLetter(playerSelection);
+    formattedComputerSelection = capitalizeFirstLetter(computerSelection);
+
+    if (formattedRoundResult == 'Win') {
+        return `You ${formattedRoundResult}! ${formattedPlayerSelection} beats ${formattedComputerSelection}.`;
+    } else if (formattedRoundResult == 'Lose') {
+        return `You ${formattedRoundResult}! ${formattedComputerSelection} beats ${formattedPlayerSelection}.`;
+    } else {
+        return `No one wins, it's a ${formattedRoundResult}! You both chose ${formattedPlayerSelection}.`
+    }
 }
+
+function game() {
+    //Play 5 round game of Rock Paper Scissors
+    let playerScore = 0;
+    let computerScore = 0;
+    while(playerScore < 3 && computerScore < 3) {
+        //get choices
+        let playerSelection = prompt('Enter your choice:');
+        let computerSelection = computerPlay();
+
+        //update scores
+        let roundResult = getRoundResult(playerSelection, computerSelection);
+        if (roundResult == 'win') playerScore++;
+        if (roundResult == 'lose') computerScore++;
+
+        //output to console
+        console.log(playRound(playerSelection, computerSelection));
+        console.log(`Player: ${playerScore}, CPU: ${computerScore}`)
+    }
+    playerScore > computerScore ? alert('You won against the CPU :)') : alert('You lost against the CPU :(')
+}
+
+//run the game
+game()
